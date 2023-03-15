f384c7a66677f794cdc37a8d380c154e5d8c0ee2
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-15 21:41:43 +0000

    added event listener for stdin

--- play.js
+++ play.js
@@ -5,11 +5,26 @@ const connect = require('./client'); //connect function
 console.log('Connecting ...');
 connect();
 
-// // setup interface to handle user input from stdin
-// const setupInput = function () {
-//   const stdin = process.stdin;
-//   stdin.setRawMode(true);
-//   stdin.setEncoding("utf8");
-//   stdin.resume();
-//   return stdin;
-// };
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  
+  // if user types input handleUserInput will run
+  stdin.on("data", handleUserInput);
+  
+  return stdin;
+
+
+};
+
+const handleUserInput = function(key) {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
