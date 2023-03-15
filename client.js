8046d9cce20677d658b1c7a7d97c3744b46171e2
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-15 21:25:34 +0000

    connected to lighthouse server

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const net = require('net'); //net module
 //establish connection with game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '10.0.2.15',// IP address
+    host: '165.227.47.243',// IP address
     port: 50541 //PORT number
   });
 
--- play.js
+++ play.js
@@ -1,6 +1,15 @@
 //Snake game - client side
+const net = require('net'); //net module
 const connect = require('./client'); //connect function
 
 console.log('Connecting ...');
 connect();
 
+// // setup interface to handle user input from stdin
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   return stdin;
+// };
