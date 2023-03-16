2e083108d40d88c4c26b4859a44cff5b324f6f47
Tara MacKinnon <taramackinnon4@gmail.com>
2023-03-16 00:03:05 +0000

    refactor to use constants and have fewer if statements

--- input.js
+++ input.js
@@ -12,44 +12,24 @@ const setupInput = (conn) => {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-
-  //what actions to take when user types
-  const handleUserInput = (key) => {
-    // \u0003 maps to ctrl+c input
-    if (key === '\u0003') {
-      process.exit();
-    }
-    //up down left right movement
-    if (key === 'w') {
-      conn.write('Move: up')
-    }
-    if (key === 'a') {
-      conn.write('Move: left');
-    }
-    if (key === 's') {
-      conn.write('Move: down');
-    }
-    if (key === 'd') {
-      conn.write('Move: right');
-    }
-    // keys that send canned messages to the server
-    if (key === 'h') {
-      conn.write('Say: Hello!');
-    }
-    if (key === 'm') {
-      conn.write('Say: Muahaha');
-    }
-    if (key === 'g') {
-      conn.write('Say: Goodbye Snakesssss');
-    }
-};
-
-  // if user types input handleUserInput will run
-  stdin.on("data", handleUserInput);
-
+  
+  // if user types input then will run callback that runs handleUserInput
+  stdin.on("data", (key) => handleUserInput(key));
 
   return stdin;
 };
 
+//what actions to take when user types
+const handleUserInput = (key) => {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  }
+  //use movements object to refactor 
+  //ex: if movements['w'] is true then do this
+  if(MOVEMENTS[key]) connection.write(`Move: ${MOVEMENTS[key]}`)
 
+  //use messages obj to refactor
+  if (MESSAGES[key]) connection.write(`Say: ${MESSAGES[key]}`)
+};
 module.exports = { setupInput };
